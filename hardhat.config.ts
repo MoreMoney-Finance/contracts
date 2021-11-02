a64d9419350de241ff79284af8ab20990efb8fdb
Gabriel Pickard <wergomat@gmail.com>
2021-11-02 00:51:03 -0500

    mm secret

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -103,7 +103,7 @@ subtask(TASK_NODE_SERVER_READY).setAction(async (args, hre, runSuper) => {
 task('print-network', 'Print network name', async (args, hre) => console.log(hre.network.name));
 
 const homedir = require('os').homedir();
-const privateKey = fs.readFileSync(`${homedir}/.marginswap-secret`).toString().trim();
+const privateKey = fs.readFileSync(`${homedir}/.moremoney-secret`).toString().trim();
 function infuraUrl(networkName: string) {
   // return `https://eth-${networkName}.alchemyapi.io/v2/AcIJPH41nagmF3o1sPArEns8erN9N691`;
   return `https://${networkName}.infura.io/v3/ae52aea5aa2b41e287d72e10b1175491`;
