d342a256d0447fab437eaf236331d0034bda8d42
Gabriel Pickard <wergomat@gmail.com>
2022-01-17 08:42:30 -0600

    flash liquidation changes

--- setup-localhost.sh
+++ setup-localhost.sh
@@ -3,13 +3,7 @@ mkdir -p ./deployments/localhost
 rsync -avu "./deployments/avalanche/" "./deployments/localhost"
 echo 31337 > ./deployments/localhost/.chainId
 
-echo "Re-deploying:"
-echo "$@"
-
-for var in "$@"
-do
-    rm ./deployments/localhost/"$var".json
-done
+./delete-deployments.sh localhost "$@"
 
 yarn hardhat deploy --network localhost
 ./export-addresses.sh localhost
