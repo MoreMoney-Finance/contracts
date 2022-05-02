2ba22ae28f09421dbc5272d5f450edb7c21b2665
Gabriel Pickard <wergomat@gmail.com>
2022-05-01 23:44:56 -0500

    Update Migrate.sol

--- contracts/Migrate.sol
+++ contracts/Migrate.sol
@@ -5,8 +5,9 @@ import "./roles/RoleAware.sol";
 import "./roles/DependsOnTrancheIDService.sol";
 import "./roles/DependsOnIsolatedLending.sol";
 import "./roles/DependsOnStableLending.sol";
+import "./Roles/DependsOnStableCoin.sol";
 
-contract Migrate is DependsOnTrancheIDService, DependsOnIsolatedLending, DependsOnStableLending, RoleAware {
+contract Migrate is DependsOnTrancheIDService, DependsOnIsolatedLending, DependsOnStableLending, DependsOnStableCoin, RoleAware {
     constructor(address roles) RoleAware(roles) {
         _rolesPlayed.push(TRANCHE_TRANSFERER);
         _rolesPlayed.push(MINTER_BURNER);
@@ -15,20 +16,24 @@ contract Migrate is DependsOnTrancheIDService, DependsOnIsolatedLending, Depends
 
     function migratePosition(uint256 trancheId, address targetStrategy) external {
         TrancheIDService tids = trancheIdService();
+        Stablecoin stable = stableCoin();
         address sourceLendingAddress = tids.viewTrancheContractByID(trancheId);
         require (sourceLendingAddress == address(isolatedLending()) || sourceLendingAddress == address(stableLending()), "Not a migratable contract");
 
         StableLending sourceLending = StableLending(sourceLendingAddress);
 
-        // TODO get collateralAmount & repayAmount from sourceLending
+        // TODO get collateralAmount & repayAmount from sourceLending (collateral and debt associated with position, you can follow the way we get position metadata on frontend)
+
         // mint sufficient stablecoin to repay
+        // stable.mint(address(this), repayAmount);
         // sourceLending.repayAndWithdraw(trancheId, collateralAmount, repayAmount, address(this));
         
-        // TODO update collateralAmount to actual collateral received (by checking balance)
-        // and set debt = 999 * (repayAmount - stable.balanceOf(address(this))) / 1000
+        // TODO update collateralAmount to actual collateral received (by checking current balance in collateral token -- TODO get collateral token for position)
+        // uint256 debt = 999 * (repayAmount - stable.balanceOf(address(this))) / 1000
         
-
-        // uint256 newTrancheId = stableLending2.mintDepositAndBorrow(targetStrategy, collateralAmount, debtAmount, address(this));
-        // stableLending2.safeTransferFrom(address(this), sourceLending.ownerOf(trancheId), newTrancheId);
+        // TODO Make sure you have created & imported DependsOnStableLending2 to run the following:
+        // StableLending2 ending2 = stableLending2();
+        // uint256 newTrancheId = lending2.mintDepositAndBorrow(targetStrategy, collateralAmount, debt, address(this));
+        // lending2.safeTransferFrom(address(this), sourceLending.ownerOf(trancheId), newTrancheId);
     }
 }
