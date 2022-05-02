9de995e4db8cad1cf57912b1e3b914f93fe4525d
Bruno Eleodoro Roza <brunoeleodoro96@gmail.com>
2022-05-02 19:04:28 -0300

    #135 Migrate Position Progress so far
    https://github.com/MoreMoney-Finance/contracts/issues/135

--- contracts/Migrate.sol
+++ contracts/Migrate.sol
@@ -5,35 +5,73 @@ import "./roles/RoleAware.sol";
 import "./roles/DependsOnTrancheIDService.sol";
 import "./roles/DependsOnIsolatedLending.sol";
 import "./roles/DependsOnStableLending.sol";
-import "./Roles/DependsOnStableCoin.sol";
+import "./roles/DependsOnStableLending2.sol";
+import "./StableLending2.sol";
+import "./roles/DependsOnStableCoin.sol";
 
-contract Migrate is DependsOnTrancheIDService, DependsOnIsolatedLending, DependsOnStableLending, DependsOnStableCoin, RoleAware {
+contract Migrate is
+    DependsOnTrancheIDService,
+    DependsOnIsolatedLending,
+    DependsOnStableLending,
+    DependsOnStableLending2,
+    DependsOnStableCoin,
+    RoleAware
+{
     constructor(address roles) RoleAware(roles) {
         _rolesPlayed.push(TRANCHE_TRANSFERER);
         _rolesPlayed.push(MINTER_BURNER);
-
     }
 
-    function migratePosition(uint256 trancheId, address targetStrategy) external {
+    function migratePosition(uint256 trancheId, address targetStrategy)
+        external
+    {
         TrancheIDService tids = trancheIdService();
         Stablecoin stable = stableCoin();
         address sourceLendingAddress = tids.viewTrancheContractByID(trancheId);
-        require (sourceLendingAddress == address(isolatedLending()) || sourceLendingAddress == address(stableLending()), "Not a migratable contract");
+        require(
+            sourceLendingAddress == address(isolatedLending()) ||
+                sourceLendingAddress == address(stableLending()),
+            "Not a migratable contract"
+        );
 
         StableLending sourceLending = StableLending(sourceLendingAddress);
 
-        // TODO get collateralAmount & repayAmount from sourceLending (collateral and debt associated with position, you can follow the way we get position metadata on frontend)
-
+        // get collateralAmount & repayAmount from sourceLending
+        (
+            uint256 collateralValue,
+            address token,
+            uint256 collateral,
+            uint256 debt,
+            uint256 yield
+        ) = sourceLending.viewPositionMetadata(trancheId);
+        uint256 repayAmount = debt > yield ? debt - yield : 0;
         // mint sufficient stablecoin to repay
-        // stable.mint(address(this), repayAmount);
-        // sourceLending.repayAndWithdraw(trancheId, collateralAmount, repayAmount, address(this));
-        
-        // TODO update collateralAmount to actual collateral received (by checking current balance in collateral token -- TODO get collateral token for position)
-        // uint256 debt = 999 * (repayAmount - stable.balanceOf(address(this))) / 1000
-        
-        // TODO Make sure you have created & imported DependsOnStableLending2 to run the following:
-        // StableLending2 ending2 = stableLending2();
-        // uint256 newTrancheId = lending2.mintDepositAndBorrow(targetStrategy, collateralAmount, debt, address(this));
-        // lending2.safeTransferFrom(address(this), sourceLending.ownerOf(trancheId), newTrancheId);
+        stable.mint(address(this), repayAmount);
+        sourceLending.repayAndWithdraw(
+            trancheId,
+            collateralValue,
+            repayAmount,
+            address(this)
+        );
+
+        collateralValue = IERC20(token).balanceOf(address(this));
+
+        // TODO update collateralValue to actual collateral received (by checking current balance in collateral token -- TODO get collateral token for position)
+        uint256 debt2 = (999 *
+            (repayAmount - stable.balanceOf(address(this)))) / 1000;
+
+        StableLending2 lending2 = stableLending2();
+        uint256 newTrancheId = lending2.mintDepositAndBorrow(
+            token,
+            targetStrategy,
+            collateralValue,
+            debt2,
+            address(this)
+        );
+        lending2.safeTransferFrom(
+            address(this),
+            sourceLending.ownerOf(trancheId),
+            newTrancheId
+        );
     }
 }
