b902ad6daa6866fb48642471027a3ccf3a510399
Gabriel Pickard <wergomat@gmail.com>
2022-06-27 23:33:05 -0500

    MasterMore edits (get rid of adjusted allocPoints)

--- contracts/governance/MasterMore.sol
+++ contracts/governance/MasterMore.sol
@@ -10,15 +10,14 @@ import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "./Math.sol";
 import "./SafeOwnableUpgradeable.sol";
-import "../../interfaces/IAssetERC20.sol";
 import "../../interfaces/IVeMore.sol";
 import "../../interfaces/IMasterMore.sol";
 import "../../interfaces/IRewarder.sol";
 import "hardhat/console.sol";
 
 /// MasterMore is a boss. He says "go f your blocks maki boy, I'm gonna use timestamp instead"
-/// In addition, he feeds himself from Venom. So, veMore holders boost their (non-dialuting) emissions.
-/// This contract rewards users in function of their amount of lp staked (dialuting pool) factor (non-dialuting pool)
+/// In addition, veMore holders boost their (non-diluting) emissions.
+/// This contract rewards users in function of their amount of lp staked (diluting pool) factor (non-diluting pool)
 /// Factor and sumOfFactors are updated by contract VeMore.sol after any veMore minting/burning (veERC20Upgradeable hook).
 /// Note that it's ownable and the owner wields tremendous power. The ownership
 /// will be transferred to a governance smart contract once More is sufficiently
@@ -31,14 +30,13 @@ contract MasterMore is
     IMasterMore
 {
     using SafeERC20 for IERC20;
-    using SafeERC20 for IAssetERC20;
     using EnumerableSet for EnumerableSet.AddressSet;
 
     // Info of each user.
     struct UserInfo {
         uint256 amount; // How many LP tokens the user has provided.
         uint256 rewardDebt; // Reward debt. See explanation below.
-        uint256 factor; // non-dialuting factor = sqrt (lpAmount * veMore.balanceOf())
+        uint256 factor; // non-diluting factor = sqrt (lpAmount * veMore.balanceOf())
         //
         // We do some fancy math here. Basically, any point in time, the amount of MOREs
         // entitled to a user but is pending to be distributed is:
@@ -55,15 +53,14 @@ contract MasterMore is
 
     // Info of each pool.
     struct PoolInfo {
-        IAssetERC20 lpToken; // Address of LP token contract.
-        uint256 baseAllocPoint; // How many base allocation points assigned to this pool
+        IERC20 lpToken; // Address of LP token contract.
+        uint256 allocPoint; // How many base allocation points assigned to this pool
         uint256 lastRewardTimestamp; // Last timestamp that MOREs distribution occurs.
         uint256 accMorePerShare; // Accumulated MOREs per share, times 1e12.
         IRewarder rewarder;
-        uint256 sumOfFactors; // the sum of all non dialuting factors by all of the users in the pool
+        uint256 sumOfFactors; // the sum of all non diluting factors by all of the users in the pool
         uint256 accMorePerFactorShare; // accumulated more per factor share
         // Note : beware storage collision with old MasterMore
-        uint256 adjustedAllocPoint; // Adjusted allocation points for this pool. MOREs to distribute per second.
     }
 
     // The strongest more out there (more token).
@@ -75,12 +72,12 @@ contract MasterMore is
     // MORE tokens created per second.
     uint256 public morePerSec;
     // Emissions: both must add to 1000 => 100%
-    // Dialuting emissions repartition (e.g. 300 for 30%)
-    uint256 public dialutingRepartition;
-    // Non-dialuting emissions repartition (e.g. 500 for 50%)
-    uint256 public nonDialutingRepartition;
+    // Diluting emissions repartition (e.g. 300 for 30%)
+    uint256 public dilutingRepartition;
+    // Non-diluting emissions repartition (e.g. 500 for 50%)
+    uint256 public nonDilutingRepartition;
     // Total allocation points. Must be the sum of all allocation points in all pools.
-    uint256 public totalBaseAllocPoint;
+    uint256 public totalAllocPoint;
     // The timestamp when MORE mining starts.
     uint256 public startTimestamp;
     // Info of each pool.
@@ -91,20 +88,18 @@ contract MasterMore is
     mapping(uint256 => mapping(address => UserInfo)) public userInfo;
     // Amount of claimable more the user has
     mapping(uint256 => mapping(address => uint256)) public claimableMore;
-    // Total adjusted allocation points. Must be the sum of adjusted allocation points in all pools.
-    uint256 public totalAdjustedAllocPoint;
     // The maximum number of pools, in case updateFactor() exceeds block gas limit
     uint256 public maxPoolLength;
 
     event Add(
         uint256 indexed pid,
-        uint256 baseAllocPoint,
-        IAssetERC20 indexed lpToken,
+        uint256 allocPoint,
+        IERC20 indexed lpToken,
         IRewarder indexed rewarder
     );
     event Set(
         uint256 indexed pid,
-        uint256 baseAllocPoint,
+        uint256 allocPoint,
         IRewarder indexed rewarder,
         bool overwrite
     );
@@ -126,8 +121,8 @@ contract MasterMore is
     event UpdateEmissionRate(address indexed user, uint256 morePerSec);
     event UpdateEmissionRepartition(
         address indexed user,
-        uint256 dialutingRepartition,
-        uint256 nonDialutingRepartition
+        uint256 dilutingRepartition,
+        uint256 nonDilutingRepartition
     );
     event UpdateVeMORE(
         address indexed user,
@@ -145,7 +140,7 @@ contract MasterMore is
         IERC20 _more,
         IVeMore _veMore,
         uint256 _morePerSec,
-        uint256 _dialutingRepartition,
+        uint256 _dilutingRepartition,
         uint256 _startTimestamp
     ) public initializer {
         require(address(_more) != address(0), "more address cannot be zero");
@@ -155,8 +150,8 @@ contract MasterMore is
         );
         require(_morePerSec != 0, "more per sec cannot be zero");
         require(
-            _dialutingRepartition <= 1000,
-            "dialuting repartition must be in range 0, 1000"
+            _dilutingRepartition <= 1000,
+            "diluting repartition must be in range 0, 1000"
         );
 
         __Ownable_init();
@@ -166,8 +161,8 @@ contract MasterMore is
         more = _more;
         veMore = _veMore;
         morePerSec = _morePerSec;
-        dialutingRepartition = _dialutingRepartition;
-        nonDialutingRepartition = 1000 - _dialutingRepartition;
+        dilutingRepartition = _dilutingRepartition;
+        nonDilutingRepartition = 1000 - _dilutingRepartition;
         startTimestamp = _startTimestamp;
         maxPoolLength = 50;
     }
@@ -202,12 +197,12 @@ contract MasterMore is
 
     /// @notice Add a new lp to the pool. Can only be called by the owner.
     /// @dev Reverts if the same LP token is added more than once.
-    /// @param _baseAllocPoint allocation points for this LP
+    /// @param _allocPoint allocation points for this LP
     /// @param _lpToken the corresponding lp token
     /// @param _rewarder the rewarder
     function add(
-        uint256 _baseAllocPoint,
-        IAssetERC20 _lpToken,
+        uint256 _allocPoint,
+        IERC20 _lpToken,
         IRewarder _rewarder
     ) public onlyOwner {
         require(
@@ -231,37 +226,34 @@ contract MasterMore is
             : startTimestamp;
 
         // update alloc point
-        uint256 adjustedAllocPoint = _baseAllocPoint;
-        totalBaseAllocPoint += _baseAllocPoint;
-        totalAdjustedAllocPoint += adjustedAllocPoint;
+        totalAllocPoint += _allocPoint;
 
         // update PoolInfo with the new LP
         poolInfo.push(
             PoolInfo({
                 lpToken: _lpToken,
-                baseAllocPoint: _baseAllocPoint,
+                allocPoint: _allocPoint,
                 lastRewardTimestamp: lastRewardTimestamp,
                 accMorePerShare: 0,
                 rewarder: _rewarder,
                 sumOfFactors: 0,
-                accMorePerFactorShare: 0,
-                adjustedAllocPoint: adjustedAllocPoint
+                accMorePerFactorShare: 0
             })
         );
 
         // add lpToken to the lpTokens enumerable set
         lpTokens.add(address(_lpToken));
-        emit Add(poolInfo.length - 1, _baseAllocPoint, _lpToken, _rewarder);
+        emit Add(poolInfo.length - 1, _allocPoint, _lpToken, _rewarder);
     }
 
     /// @notice Update the given pool's MORE allocation point. Can only be called by the owner.
     /// @param _pid the pool id
-    /// @param _baseAllocPoint allocation points
+    /// @param _allocPoint allocation points
     /// @param _rewarder the rewarder
     /// @param overwrite overwrite rewarder?
     function set(
         uint256 _pid,
-        uint256 _baseAllocPoint,
+        uint256 _allocPoint,
         IRewarder _rewarder,
         bool overwrite
     ) public onlyOwner {
@@ -274,20 +266,18 @@ contract MasterMore is
 
         PoolInfo storage pool = poolInfo[_pid];
 
-        totalBaseAllocPoint =
-            totalBaseAllocPoint -
-            pool.baseAllocPoint +
-            _baseAllocPoint;
-        pool.baseAllocPoint = _baseAllocPoint;
-        // update adjustedAllocPoint point after baseAllocPoint
-        _updateAdjustedAllocPoint(pool);
+        totalAllocPoint =
+            totalAllocPoint -
+            pool.allocPoint +
+            _allocPoint;
+        pool.allocPoint = _allocPoint;
 
         if (overwrite) {
             pool.rewarder = _rewarder;
         }
         emit Set(
             _pid,
-            _baseAllocPoint,
+            _allocPoint,
             overwrite ? _rewarder : pool.rewarder,
             overwrite
         );
@@ -317,13 +307,13 @@ contract MasterMore is
             uint256 secondsElapsed = block.timestamp - pool.lastRewardTimestamp;
             uint256 moreReward = (secondsElapsed *
                 morePerSec *
-                pool.adjustedAllocPoint) / totalAdjustedAllocPoint;
+                pool.allocPoint) / totalAllocPoint;
             accMorePerShare +=
-                (moreReward * 1e12 * dialutingRepartition) /
+                (moreReward * 1e12 * dilutingRepartition) /
                 (lpSupply * 1000);
             if (pool.sumOfFactors != 0) {
                 accMorePerFactorShare +=
-                    (moreReward * 1e12 * nonDialutingRepartition) /
+                    (moreReward * 1e12 * nonDilutingRepartition) /
                     (pool.sumOfFactors * 1000);
             }
         }
@@ -391,23 +381,20 @@ contract MasterMore is
             // calculate more reward
             uint256 moreReward = (secondsElapsed *
                 morePerSec *
-                pool.adjustedAllocPoint) / totalAdjustedAllocPoint;
-            // update accMorePerShare to reflect dialuting rewards
+                pool.allocPoint) / totalAllocPoint;
+            // update accMorePerShare to reflect diluting rewards
             pool.accMorePerShare +=
-                (moreReward * 1e12 * dialutingRepartition) /
+                (moreReward * 1e12 * dilutingRepartition) /
                 (lpSupply * 1000);
 
-            // update accMorePerFactorShare to reflect non-dialuting rewards
+            // update accMorePerFactorShare to reflect non-diluting rewards
             if (pool.sumOfFactors == 0) {
                 pool.accMorePerFactorShare = 0;
             } else {
                 pool.accMorePerFactorShare +=
-                    (moreReward * 1e12 * nonDialutingRepartition) /
+                    (moreReward * 1e12 * nonDilutingRepartition) /
                     (pool.sumOfFactors * 1000);
             }
-
-            // update allocation point
-            _updateAdjustedAllocPoint(pool);
 
             // update lastRewardTimestamp to now
             pool.lastRewardTimestamp = block.timestamp;
@@ -475,7 +462,7 @@ contract MasterMore is
         // update amount of lp staked by user
         user.amount += _amount;
 
-        // update non-dialuting factor
+        // update non-diluting factor
         uint256 oldFactor = user.factor;
         user.factor = Math.sqrt(user.amount * veMore.balanceOf(_user));
         pool.sumOfFactors = pool.sumOfFactors + user.factor - oldFactor;
@@ -495,54 +482,6 @@ contract MasterMore is
 
         pool.lpToken.safeTransferFrom(msg.sender, address(this), _amount);
         emit DepositFor(_user, _pid, _amount);
-    }
-
-    /// @notice update adjusted allocation point for the corresponding pool
-    /// @param pool the pool to update
-    function _updateAdjustedAllocPoint(PoolInfo storage pool) internal {
-        uint256 latestAdjustedAllocPoint = pool.baseAllocPoint *
-            _assetAdjustFactor(pool.lpToken);
-        totalAdjustedAllocPoint =
-            totalAdjustedAllocPoint +
-            latestAdjustedAllocPoint -
-            pool.adjustedAllocPoint;
-        pool.adjustedAllocPoint = latestAdjustedAllocPoint;
-    }
-
-    /// @notice get the interest adjust factor for the pool
-    /// @param pid the pool id to query
-    function poolAdjustFactor(uint256 pid) external view returns (uint256) {
-        PoolInfo memory pool = poolInfo[pid];
-        return _assetAdjustFactor(pool.lpToken);
-    }
-
-    /// @notice Get the interest adjust factor for an asset
-    /// @param asset the address of asset
-    function _assetAdjustFactor(IAssetERC20 asset)
-        internal
-        view
-        returns (uint256)
-    {
-        uint256 liability = asset.liability();
-        // if liability is 0, the default adjust factor is 0
-        uint256 r = liability == 0
-            ? 1 ether
-            : (1 ether * asset.cash()) / liability;
-        return _adjustFactor(r);
-    }
-
-    /// @notice Get the interest adjust factor by coverage ratio
-    /// @param r coverage ratio
-    function _adjustFactor(uint256 r) internal pure returns (uint256) {
-        if (r == 0) {
-            // return an infinite small number in case of division of 0;
-            return 1;
-        }
-        return
-            Math.wdiv(
-                1 ether,
-                0.2 ether + Math.wdiv(1 ether, Math.wmul(r, Math.wmul(r, r)))
-            );
     }
 
     /// @notice Deposit LP tokens to MasterChef for MORE allocation.
@@ -579,7 +518,7 @@ contract MasterMore is
         // update amount of lp staked by user
         user.amount += _amount;
 
-        // update non-dialuting factor
+        // update non-diluting factor
         uint256 oldFactor = user.factor;
         user.factor = Math.sqrt(user.amount * veMore.balanceOf(msg.sender));
         pool.sumOfFactors = pool.sumOfFactors + user.factor - oldFactor;
@@ -723,13 +662,13 @@ contract MasterMore is
         pending = safeMoreTransfer(payable(msg.sender), pending);
         emit Harvest(msg.sender, _pid, pending);
 
-        // for non-dialuting factor
+        // for non-diluting factor
         uint256 oldFactor = user.factor;
 
         // update amount of lp staked
         user.amount = user.amount - _amount;
 
-        // update non-dialuting factor
+        // update non-diluting factor
         user.factor = Math.sqrt(user.amount * veMore.balanceOf(msg.sender));
         pool.sumOfFactors = pool.sumOfFactors + user.factor - oldFactor;
 
@@ -759,11 +698,11 @@ contract MasterMore is
         UserInfo storage user = userInfo[_pid][msg.sender];
         pool.lpToken.safeTransfer(address(msg.sender), user.amount);
 
-        // update non-dialuting factor
+        // update non-diluting factor
         pool.sumOfFactors = pool.sumOfFactors - user.factor;
         user.factor = 0;
 
-        // update dialuting factors
+        // update diluting factors
         user.amount = 0;
         user.rewardDebt = 0;
 
@@ -803,19 +742,19 @@ contract MasterMore is
     }
 
     /// @notice updates emission repartition
-    /// @param _dialutingRepartition the future dialuting repartition
-    function updateEmissionRepartition(uint256 _dialutingRepartition)
+    /// @param _dilutingRepartition the future diluting repartition
+    function updateEmissionRepartition(uint256 _dilutingRepartition)
         external
         onlyOwner
     {
-        require(_dialutingRepartition <= 1000);
+        require(_dilutingRepartition <= 1000);
         massUpdatePools();
-        dialutingRepartition = _dialutingRepartition;
-        nonDialutingRepartition = 1000 - _dialutingRepartition;
+        dilutingRepartition = _dilutingRepartition;
+        nonDilutingRepartition = 1000 - _dilutingRepartition;
         emit UpdateEmissionRepartition(
             msg.sender,
-            _dialutingRepartition,
-            1000 - _dialutingRepartition
+            _dilutingRepartition,
+            1000 - _dilutingRepartition
         );
     }
 
